f87693edf6dbb747e1e0fd6908dda9b190a721f3
Карен <karen@MacBook-Pro-Karen.local>
2022-05-13 17:57:42 +0300

    add className to button

--- javascript/button.js
+++ javascript/button.js
@@ -1,9 +1,10 @@
 
 
-export const Button = (text = '') => {
+export const Button = (text = '', className = '') => {
     const button = document.createElement('button');
     button.innerText = text;
     button.classList.add('button')
+    button.classList.add(className)
 
     return button;
 }
